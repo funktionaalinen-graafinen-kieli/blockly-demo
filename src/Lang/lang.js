7e29485ba3c79bea992ebce0765bffe25832aa7f
Nicolas Hiillos <nicolashiillos@gmail.com>
2020-01-17 14:32:07 +0200

    Add helper functions and comments

--- src/Lang/lang.js
+++ src/Lang/lang.js
@@ -1,25 +1,33 @@
-class Entity {
-    constructor(x, y, t) {
-        this.x = x;
-        this.y = y;
-    }
-}
+// removes \n and runs eval
+var makeJs = s => eval(s.replace(/\/\n/g,''));
 
-var e = new Entity(1,-1);
-var t;
-
-// basic blocks
+// BASIC FUNCTIONS
 // each is a curried function
-var add = x => y => "("+x+")+("+y+")";
+var add = x => y => infix("+",x,y);
 
-var cond = b => f => g => b+"?"+f+":"+g;
+var sub = x => y => infix("-",x,y);
 
-var gt = x => y => "("+x+")>("+y+")";
+var gt = x => y => infix(">",x,y);
 
-// state handling functions
+var lt = x => y => infix("<",x,y);
 
-// p must be string
-var mutate = p => v => "eval("+p+"="+v+")";
+var cond = b => f => g => infix("?",b,infix(":",f,g));
 
-// p and f are strings
-var timer = p => f => t => mutate(p)("setInterval(eval(()=>"+f+"),"+t+")");
+// STATE HANDLING FUNCTIONS
+
+// Stores a value with a name
+// or changes existing variable.
+var mutate = name => val => "eval"+w(infix("=",name,val));
+
+// 
+var timer = name => f => t => mutate(p)(cat("setInterval(eval(()=>",f,"),",t,")"));
+
+// HELPER FUNCTIONS
+
+var infix = (op,x,y) => cat(w(x),op,w(y));
+
+// wrap in parens
+var w = x => "("+x+")";
+
+// concatinate strings
+var cat = (...xs) => xs.reduce((x,y) => String(x)+String(y),"");
