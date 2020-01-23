0b6dbd9f02450be00181eae52211c5056acf2346
Kerkko Pelttari <kerk.pelt@gmail.com>
2020-01-23 09:44:38 +0200

    Export Lang

---
+++ src/Lang/main.js
@@ -0,0 +1,12 @@
+// Class to test out lang features
+import Lang from "lang"
+
+class Entity {
+    constructor(x, y, t) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+let e = new Entity(1,-1)
+Lang.gt(e, 0)
