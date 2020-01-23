ebfa9da2e6df03f4c724aa376154ce8fbf6a3f33
Elias Lankinen <elias@lankinen.xyz>
2020-01-23 11:21:18 +0200

    image moving

--- src/GameEngine/game_engine.js
+++ src/GameEngine/game_engine.js
@@ -4,77 +4,85 @@ import Entity from "./Entity.js"
 
 const frameTime = 1000 / 60 // 60fps
 
-export default function GameEngine() {
-    const [state, setState] = useState(new Map())
-    const updateState = (k,v) => {
-        setState(state.set(k,[v[0], applyF(k,state)]))
+
+export default class GameEngine extends React.Component { 
+    constructor(props){
+        super(props)
+        this.state = {state:new Map()}
+        this.id = (x, s) => x
+        this.entity = new Entity("e", this.state.state, [[
+            "x",
+            [(x, s) => (s.get("everySecond")[1][0] ? x + 1 : x), 1]
+        ],
+        ["y", [this.id, 1]],
+        ["img", [this.id, "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/golden-retriever-dog-royalty-free-image-505534037-1565105327.jpg"]]
+        ])
     }
-
-    let getVal = name => state.get(name)[1]
-
-    const timer = (x,s) => {
-        if(getVal("time") - x[1] >= x[2]){
-            return [true,getVal("time"),x[2]]
+    getVal = name => this.state.state.get(name)[1]
+    timer = (x,s) => {
+        if(this.getVal("time") - x[1] >= x[2]){
+            return [true,this.getVal("time"),x[2]]
         } else{
             return [false,x[1],x[2]]
         }
     }
 
-    let id = (x, s) => x
+    componentDidMount(){
+        this.state.state.set("frameTime", [this.id,frameTime])
+        this.state.state.set("time", [(x,s) => x+s.get("frameTime")[1],0])
+        this.state.state.set("everySecond", [this.timer,[false,0,1000]])
+        this.run()
+    }
+
+    updateState = (k,v) => {
+        const nv = this.applyF(k,this.state.state)
+        this.setState({state:this.state.state.set(k,[v[0],this.applyF(k,this.state.state)])})
+    }
   
-    state.set("frameTime",[id,frameTime])
-    state.set("time",[(x,s) => {
-        return x+s.get("frameTime")[1]},0])
-    state.set("everySecond",[timer,[false,0,1000]])
-  
-    const entity = new Entity("e", state, [
-        [
-            "x",
-            [(x, s) => (s.get("everySecond")[1][0] ? x + 1 : x), 1]
-        ],
-        ["y", [id, 1]],
-        ["img", [id, "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/golden-retriever-dog-royalty-free-image-505534037-1565105327.jpg"]]
-    ])
-  
-    function sleep(ms) {
+    sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms))
     }
   
-    function applyF(key,state) {
+    applyF(key,state) {
         //store the pair
-        let p = state.get(key)
+        let p = this.state.state.get(key)
   
         // apply the function to its value
-        return p[0](p[1],state)
+        return p[0](p[1],this.state.state)
     }
   
-    async function run() {
-        while (true) {
-            // console.warn("x:",getVal(entity.x))
-            // iterates through all key-value pairs in state
-            for (let [key, value] of state) {
-                //console.log(key + ' = ' + value[1]); // for testing
-                // puts the new pair into nextState
-                updateState(key,value)
+    run = async () =>  {
+        setInterval(()=>{
+            for (let [key, value] of this.state.state) {
+            //console.log(key + ' = ' + value[1]); // for testing
+            // puts the new pair into nextState
+                this.updateState(key,value)
             }
-            await sleep(frameTime)
-            //await sleep(100); //for testing
-        }
+        },frameTime)
+        // while (true) {
+        //     this.setState({test:100})
+        //     console.warn("test:",test)
+        // setX(x+1)
+        // console.warn("x:",x)
+        // console.warn("x:",getVal(entity.x))
+        // iterates through all key-value pairs in state
+        // for (let [key, value] of state) {
+        //     //console.log(key + ' = ' + value[1]); // for testing
+        //     // puts the new pair into nextState
+        //     this.updateState(key,value)
+        // }
+        // await sleep(frameTime)
+        //await sleep(100); //for testing
     }
-
-    console.warn("x:",getVal(entity.x))
-  
-    useEffect(()=>{
-        run()
-    },[])
-
-    return (
-        <div>
-            <img style={{width: 500, height: 500, backgroundColor: "red", position:"absolute", left:0, top:0}}/>
-            <img
-                style={{width: 20, height: 20, position:"absolute", left: getVal(entity.x), top: getVal(entity.y)}}
-                src={getVal(entity.img)}
-            />
-        </div>
-    )
+    render(){
+    // console.warn("time:",state)
+        return (
+            <div>
+                <img style={{width: 500, height: 500, backgroundColor: "red", position:"absolute", left:0, top:0}}/>
+                <img
+                    style={{width: 20, height: 20, position:"absolute", left: this.getVal(this.entity.x), top: this.getVal(this.entity.y)}}
+                    src={this.getVal(this.entity.img)}
+                />
+            </div>
+        )}
 }
