d804f8532123f2c4e1cc270bd14962414f11bb42
Kerkko Pelttari <kerk.pelt@gmail.com>
2020-01-31 09:27:37 +0200

    Update dependencies and @types

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,4 +1,6 @@
 /// <reference types="react-scripts" />
+/// <reference path="types/blockly.d.ts" />
+
 declare module "blockly/blocks"
 declare module "blockly/msg/en"
 declare module "blockly/javascript"
