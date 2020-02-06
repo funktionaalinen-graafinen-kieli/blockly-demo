74b3087806b7661479f2d3aa692f98a0685aa484
Kerkko Pelttari <kerk.pelt@gmail.com>
2020-02-06 17:51:59 +0200

    Rename and update eval function

--- src/Lang/eval_func.js
+++ src/Lang/eval_func.js
@@ -1,17 +1,20 @@
+import * as log from "loglevel"
 // Class to test out lang features
 const {add,timer,get,id,pack,infix,cond,packF,lt,gt,sin,mul} =  require('./lang');
 
 export default function EvalFunc(flang) {
 
-    var js = JSON.parse(flang)
-    Object.keys(js["entities"]).forEach(e => {
-        Object.keys(js["entities"][e]).forEach(v => {
-            js["entities"][e][v][0] = eval(eval(js["entities"][e][v][0]))
+    let js = JSON.parse(flang)
+    Object.keys(js["entities"]).forEach(key => {
+        Object.keys(js["entities"][key]).forEach(v => {
+            const entity = js["entities"][key][v]
+            entity[0] = eval(eval(entity[0]))
         })
     })
-    Object.keys(js["events"]).forEach(e => {
-        js["events"][e][0] = eval(eval(js["events"][e][0]))
+    Object.keys(js["events"]).forEach(key => {
+        const event = js["events"][key]
+        event[0] = eval(eval(event[0]))
     })
-    console.log(js)
+    log.trace(js)
     return(js)
 }
