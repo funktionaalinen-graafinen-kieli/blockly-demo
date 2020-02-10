dd3409f2ae61edc722bb06a3d1e1f5bd13de439d
Hanna Vuorivirta <hannavuorivirta@gmail.com>
2020-02-10 12:29:53 +0200

    render on same page

--- src/index.tsx
+++ src/index.tsx
@@ -9,43 +9,15 @@ import {FunklyRenderer} from "./BlocklyEditor/BlocklyReact/funkly_renderer"
 
 import Main from "./GameEngine/main"
 
-class Adapter extends React.Component<{}, {toggle: boolean}> {
-    constructor(props: {}) {
-        super(props)
-        this.state = {toggle: false}
-    }
-
-    onClick = () => {
-        this.setState({toggle : !this.state.toggle} )
-    }
-
-    render = () => {
-        if (this.state.toggle) {
-            return (
-                <div>
-                    <button onClick={this.onClick}> press this to change component </button>
-		    <BlocklyEditor/>
-                </div>
-            )
-        } else {
-            return (
-                <div>
-                    <button onClick={this.onClick}> press this to change component </button>
-                    <Main/>
-                </div>
-            )
-        }
-    }
-}
-
 log.setLevel("trace")
 
 //blockRendering.unregister("geras")
 blockRendering.register("funkly_renderer", FunklyRenderer)
 
 ReactDOM.render(
-    <Adapter/>,
-    document.getElementById("root")
+    <React.Fragment>
+        <Main/>
+        <BlocklyEditor/>
+    </React.Fragment>
+    , document.getElementById("root")
 )
-
-
