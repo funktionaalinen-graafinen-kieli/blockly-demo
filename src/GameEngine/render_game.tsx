65e2f4eb68300a134ddcab4bc816f9f37d68ef7c
Kerkko Pelttari <kerk.pelt@gmail.com>
2020-02-14 11:10:40 +0200

    Split game engine into render and engine

---
+++ src/GameEngine/render_game.tsx
@@ -0,0 +1,56 @@
+import * as React from "react"
+import { Row, Col } from "react-bootstrap"
+
+import { StateMap } from "./state_map"
+import { posFactor, imgSize, gameboard } from "./config"
+import { clamp } from "./utils"
+import GameEngine from "./game_engine"
+import Entity from "./entity"
+
+
+export const renderGame = (gameEngine: GameEngine) => {
+
+    return (
+        <Row>
+            <Col>
+                <div
+                    style={gameboard["containerStyle"]}
+                    ref={gameEngine.gameArea}
+                    onKeyDown={gameEngine.handleKeyDown}
+                    onKeyUp={gameEngine.handleKeyUp}
+                    tabIndex={0}
+                >
+                    {gameEngine.state.entities.map((entity: Entity, key) => (
+                        <div key={key}>
+                            <img
+                                style={{
+                                    width: imgSize["width"],
+                                    height: imgSize["height"],
+                                    position: "absolute",
+                                    left: clamp(
+                                        // @ts-ignore
+                                        window.innerWidth * (gameEngine.getVal(entity.x) * posFactor),
+                                        0,
+                                        gameboard["size"]["width"]
+                                    ),
+                                    top: clamp(
+                                        // @ts-ignore
+                                        window.innerHeight * (gameEngine.getVal(entity.y) * posFactor),
+                                        0,
+                                        gameboard["size"]["height"]
+                                    )
+                                }}
+                                // @ts-ignore
+                                src={gameEngine.getVal(entity.img)}
+                                alt="loading..."
+                            />
+                        </div>
+                    ))}
+                </div>
+            </Col>
+            <Col>
+                <StateMap gameState={gameEngine.state.gameState} />
+            </Col>
+        </Row>
+    )
+}
