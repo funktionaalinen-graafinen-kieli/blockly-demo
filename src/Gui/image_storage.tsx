15cd22a8719d96e4133182efceafb00980601c66
Nicolas Hiillos <nicolashiillos@gmail.com>
2020-02-20 09:56:49 +0200

    Add animated gif

--- src/Gui/image_storage.tsx
+++ src/Gui/image_storage.tsx
@@ -1,5 +1,6 @@
 import doge from "./assets/shoudoge.png"
 import breadoge from "./assets/breadoge.png"
+import dogedance from "./assets/dogedance.gif"
 import muscledoge from "./assets/muscledoge.png"
 import strangedoge from "./assets/strangedoge.png"
 
@@ -8,5 +9,6 @@ export const publicImages = new Map([
     ["bread_doge", breadoge],
     ["muscle_doge", muscledoge],
     ["doge", doge],
+    ["dancingdoge", dogedance],
     ["strange_doge", strangedoge]
 ])
