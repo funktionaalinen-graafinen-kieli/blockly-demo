6f64fc8196ca831b8247cf8db73ace63f5ecdf4d
Hanna Vuorivirta <hannavuorivirta@gmail.com>
2020-02-22 12:21:20 +0200

    missing comma

--- src/Gui/image_storage.tsx
+++ src/Gui/image_storage.tsx
@@ -3,14 +3,11 @@ import breadoge from "./assets/breadoge.png"
 import dogedance from "./assets/dogedance.gif"
 import muscledoge from "./assets/muscledoge.png"
 import strangedoge from "./assets/strangedoge.png"
-<<<<<<< HEAD
 import plasticbag from "./assets/plasticbag.png"
 import turtle from "./assets/turtle.png"
 import jellyfish from "./assets/jellyfish.gif"
-=======
 import sininen from "./assets/sininen.svg"
 
->>>>>>> 04934a0ba922f41b66f4c9cf1b4aad7dd1af9d9f
 
 export const publicImages = new Map([
     ["bread_doge", breadoge],
@@ -18,11 +15,8 @@ export const publicImages = new Map([
     ["doge", doge],
     ["dancingdoge", dogedance],
     ["strange_doge", strangedoge],
-<<<<<<< HEAD
     ["plastic_bag", plasticbag],
     ["turtle", turtle],
-    ["jellyfish", jellyfish]
-=======
+    ["jellyfish", jellyfish],
     ["sininen_palikka", sininen]
->>>>>>> 04934a0ba922f41b66f4c9cf1b4aad7dd1af9d9f
 ])
