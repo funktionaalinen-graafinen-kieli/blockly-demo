63bdc67c92a34450f9bffa39e2355b1507e8bc98
Elias Lankinen <elias@lankinen.xyz>
2020-02-23 18:34:34 +0200

    tests

--- src/GameEngine/__tests__/game_engine.js
+++ src/GameEngine/__tests__/game_engine.js
@@ -3,32 +3,21 @@ import "@testing-library/jest-dom/extend-expect"
 import React from "react"
 import { render, fireEvent, screen, cleanup } from "@testing-library/react"
 
-import GameEngine, {MapWithDefault} from "../game_engine"
+import GameEngine, { MapWithDefault } from "../game_engine"
 
 afterEach(cleanup)
 
-test('MapWithDefault',()=>{
+test("MapWithDefault",()=>{
     const defaultFunc = () => [(x, s) => x, false]
     const testMap = new MapWithDefault(defaultFunc)
-    testMap.set('a','hello world')
-    testMap.set('b','abc')
-    expect(testMap.get('a')).toEqual('hello world')
-    expect(testMap.get('c')[0](1,0)).toEqual(1)
-    expect(testMap.get('c')[1]).toEqual(false)
+    testMap.set("a","hello world")
+    testMap.set("b","abc")
+    expect(testMap.get("a")).toEqual("hello world")
+    expect(testMap.get("c")[0](1,0)).toEqual(1)
+    expect(testMap.get("c")[1]).toEqual(false)
 })
 
-// test('Incorrect code breaks', () => {
-//     render(<GameEngine
-//         toggle={true}
-//         program={'{d}'}
-//         updater={async () => setInterval((updatee) => {
-//             updatee.update()
-//         }, 1000)}
-//     />)
-
-// })
-
-test('GameEngine',() => {
+test("GameEngine",() => {
     const ge = new GameEngine({
         "entities": {
             "e1": {
@@ -47,7 +36,9 @@ test('GameEngine',() => {
         log.debug("Interval update happening")
         updatee.update()
     }, 1000))
-    console.warn('GameEngine:',ge.getVal('xs'))
-    // expect(screen).toMatchSnapshot();
+    // console.warn("gameengine:",ge)
+    // ge.applyF("a",ge.state.gameState)
+    // console.warn("GameEngine:",ge)
+    expect(ge.getVal("abc")).toBe(false)
 })
 
