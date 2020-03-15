878eb2cd460932a1fbce8ceb46616b446a377668
Nicolas Hiillos <nicolashiillos@gmail.com>
2020-03-15 19:26:59 +0200

    Custom type-based notches

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,14 @@ import ReactDOM from "react-dom"
 import * as log from "loglevel"
 import { blockRendering } from "blockly"
 
-import { FunklyRenderer } from "./BlocklyEditor/BlocklyReact/funkly_renderer"
+import { TypedConnectionShapeRenderer } from "./BlocklyEditor/BlocklyReact/funkly_renderer"
 
 import App from "./Gui/app"
 
 log.setLevel("trace")
 
 //blockRendering.unregister("geras")
-blockRendering.register("funkly_renderer", FunklyRenderer)
+blockRendering.register("funkly_renderer", TypedConnectionShapeRenderer)
 
 ReactDOM.render(
     <App />, document.getElementById("root")
