cfff4c0afb51dee6130026cf24425201e7fb07e6
Kerkko Pelttari <kerk.pelt@gmail.com>
2020-03-20 10:29:31 +0200

    Fix packF calls in test program

--- src/GameEngine/__tests__/game_engine.js
+++ src/GameEngine/__tests__/game_engine.js
@@ -11,21 +11,21 @@ import { MapWithDefault } from "../utils"
 const testProgram = `{
     "entities": {
         "4pid7|lk3wy)ahaslre": {
-            "name": [ "packf(id)", "esimerkkinimi" ],
+            "name": [ "packF(id)", "esimerkkinimi" ],
             "x": [ "pack(clamp(  get('4pid7|lk3wy)ahaslre_x'))(0)(600))", 1 ],
             "y": [ "pack(clamp(  get('4pid7|lk3wy)ahaslre_y'))(0)(400))", 1 ],
-            "w": [ "packf(id)", 60 ],
-            "h": [ "packf(id)", 60 ],
-            "r": [ "packf(id)", 30 ],
+            "w": [ "packF(id)", 60 ],
+            "h": [ "packF(id)", 60 ],
+            "r": [ "packF(id)", 30 ],
             "text": [ "pack('')", "" ],
             "img": ["pack('/static/media/default_image.5d478a5d.png')","/static/media/default_image.5d478a5d.png"]
         }
     },
     "binds": {
-        "frametime": [ "packf(id)", 16 ],
+        "frametime": [ "packF(id)", 16 ],
         "time": [ "pack(add(get('time'))(get('frametime')))", 0 ],
         "random": [ "(x,s) => math.random()", 0 ],
-        "everysecond": [ "packf(timer)", [ false, 0, 1000 ] ]
+        "everysecond": [ "packF(timer)", [ false, 0, 1000 ] ]
     }
 }
 `
