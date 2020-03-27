9513ef653b01592746bbca88b7f42651fa30b026
Kerkko Pelttari <kerk.pelt@gmail.com>
2020-03-27 09:14:33 +0200

    Add hard coded eneity list to character selector

--- src/BlocklyEditor/character_selector.tsx
+++ src/BlocklyEditor/character_selector.tsx
@@ -1,6 +1,35 @@
 import React from "react"
 
+const entityMap = {
+    "entities": {
+        "J|*C80I]n-iaRWfN[9B1": {
+            "name": [
+                "packF(id)",
+                "kilpikonna"
+            ],
+            "img": [
+                "pack(  '\"/static/media/turtle.fda1f442.png\"')",
+                "/static/media/default_image.5d478a5d.png"
+            ]
+        },
+        "]h;BXQeK[(LG98GJ4~2M": {
+            "name": [
+                "packF(id)",
+                "muovipussi"
+            ],
+            "img": [
+                "pack(  '\"/static/media/plasticbag.7e82159b.png\"')",
+                "/static/media/default_image.5d478a5d.png"
+            ]
+        }
+    }
+}
+
 const CharacterSelector = () => {
+    const createNewEntity = () => {
+        
+    }
+
     return null
 }
 
