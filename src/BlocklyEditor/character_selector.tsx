49bd082fe36c319b67e172c397b890f708b1cad3
Kerkko Pelttari <kerk.pelt@gmail.com>
2020-04-06 09:53:08 +0300

    add plus and delete button assets

--- src/BlocklyEditor/character_selector.tsx
+++ src/BlocklyEditor/character_selector.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import Blockly from "blockly"
 
+import { guiImages } from "../Gui/image_storage"
 import Editor from "../BlocklyEditor/editor"
 
 const entityMap = {
@@ -32,29 +33,29 @@ const entityMap = {
     }
 }
 
-const entityBaseXml = (id: string) => `<xml xmlns=\"https://developers.google.com/blockly/xml\">
-    <block type=\"funkly_entity\" id=\"${id}\" x=\"420\" y=\"239\">
-    <field name=\"name\">esimerkkinimi</field>
-    <field name=\"initx\">1</field>
-    <field name=\"inity\">1</field>
-    <field name=\"width\">60</field>
-    <field name=\"height\">60</field>
-    <field name=\"radius\">60</field>
-    <statement name=\"x\">
-      <shadow type=\"funkly_get\" id=\"O]RBe)x282zy]s-g[^3P\">
-        <field name=\"entity\">NOT_SELECTED</field>
-        <field name=\"property\">x</field>
+const entityBaseXml = (id: string) => `<xml xmlns="https://developers.google.com/blockly/xml">
+    <block type="funkly_entity" id="${id}" x="420" y="239">
+    <field name="name">esimerkkinimi</field>
+    <field name="initx">1</field>
+    <field name="inity">1</field>
+    <field name="width">60</field>
+    <field name="height">60</field>
+    <field name="radius">60</field>
+    <statement name="x">
+      <shadow type="funkly_get" id="O]RBe)x282zy]s-g[^3P">
+        <field name="entity">NOT_SELECTED</field>
+        <field name="property">x</field>
       </shadow>
     </statement>
-    <statement name=\"y\">
-      <shadow type=\"funkly_get\" id=\"INhqa*+n8.,,gvgJYd3z\">
-        <field name=\"entity\">NOT_SELECTED</field>
-        <field name=\"property\">y</field>
+    <statement name="y">
+      <shadow type="funkly_get" id="INhqa*+n8.,,gvgJYd3z">
+        <field name="entity">NOT_SELECTED</field>
+        <field name="property">y</field>
       </shadow>
     </statement>
-    <statement name=\"img\">
-      <shadow type=\"funkly_img\" id=\"Fz#TNaasKi!WPddKz%Gx\">
-        <field name=\"IMAGE\">/static/media/default_image.a25c40e5.png</field>
+    <statement name="img">
+      <shadow type="funkly_img" id="Fz#TNaasKi!WPddKz%Gx">
+        <field name="IMAGE">/static/media/default_image.a25c40e5.png</field>
       </shadow>
     </statement>
   </block>
@@ -71,10 +72,10 @@ const CharacterCard = (props: CharacterCardProps) => {
     return (
         <div className="funkly-character-card">
             <img
-                src={"https://cdn2.iconfinder.com/data/icons/minimal-4/100/close-512.png"}
+                src={guiImages.get("deleteButton")}
                 alt="..."
                 style={{ position: "absolute", height: 20, width: 20 }}
-                onClick={props.delete}
+                onClick={props.delete(props.name)}
             />
             <img src={props.img} alt="..." style={{ height: 50, width: 50 }} />
             <p>{props.name}</p>
@@ -172,7 +173,7 @@ const CharacterSelector = (props: CharacterSelectorProps) => {
                         )
                     })}
                     <img
-                        src="https://cdn4.iconfinder.com/data/icons/media-buttons-1/200/761-512.png"
+                        src={guiImages.get("plusButton")}
                         width={75}
                         height={75}
                         style={{ position: "absolute", right: 0, bottom: 0 }}
--- src/Gui/image_storage.tsx
+++ src/Gui/image_storage.tsx
@@ -19,6 +19,8 @@ import debugon from "./assets/gui_images/debugon.png"
 import debugoff from "./assets/gui_images/debugoff.png"
 import xmlbutton from "./assets/gui_images/xmlbutton.png"
 import choosefile from "./assets/gui_images/choosefile.png"
+import plusbutton from "./assets/gui_images/plusbutton.png"
+import deletebutton from "./assets/gui_images/deletebutton.png"
 
 export const entityImages = new Map([
     ["default_image", default_image],
@@ -44,5 +46,7 @@ export const guiImages = new Map([
     ["debugon", debugon],
     ["debugoff", debugoff],
     ["xml", xmlbutton],
-    ["choosefile", choosefile]
+    ["choosefile", choosefile],
+    ["plusButton", plusbutton],
+    ["deleteButton", deletebutton]
 ])
