8a540782993af587f75bbb60047abb433d24dbea
Kerkko Pelttari <kerk.pelt@gmail.com>
2020-04-13 10:43:44 +0300

    Useful prints for figuring out how to improve mouse hover coords

--- src/Gui/mouse_location.tsx
+++ src/Gui/mouse_location.tsx
@@ -5,6 +5,8 @@ export const MouseLocation = (props: {children?: React.ReactNode}) => {
     const [mouseX, setMouseX] = React.useState(0)
     const [mouseY, setMouseY] = React.useState(0)
     const hoverAction = (event: React.MouseEvent<HTMLDivElement>) => {
+        const [x, y] = [event.clientX, event.clientY]
+        console.log(`x: ${x}, y: ${y}`)
         const [newX, newY] = [event.nativeEvent.offsetX, event.nativeEvent.offsetY]
         setMouseX(newX)
         setMouseY(newY)
@@ -12,7 +14,7 @@ export const MouseLocation = (props: {children?: React.ReactNode}) => {
     return (
         <div onMouseMove={hoverAction}>
             <div className="funkly-mouse-location">
-            <img className="funkly-mouse-icon" src={guiImages.get("mouseicon")} alt="mouseicon" />
+                <img className="funkly-mouse-icon" src={guiImages.get("mouseicon")} alt="mouseicon" />
                 {mouseX}, {mouseY}
             </div>
             {props.children}
